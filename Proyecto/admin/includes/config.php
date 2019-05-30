d85e2df1efe8ea1bb0a2f27f5c1ba56eaf793de9
Dperaltat <peraltadaniel069@gmail.com>
2019-05-30 00:16:47 -0500

    Conectado a Cloud

--- Proyecto/admin/includes/config.php
+++ Proyecto/admin/includes/config.php
@@ -13,8 +13,7 @@
 |
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_USER', 'id9766219_hipermedial' );             // Set database user
+  define( 'DB_PASS', '12345' );             // Set database password
   define( 'DB_NAME', 'id9766219_pizzahome' );        // Set database name
-
 ?>

0c128ab582485f6a19efc1e2102c247ff57e427d
Wilmer <oscardurasno12@gmail.com>
2019-05-31 14:12:20 -0500

    modificacion de estilos, tambien se agrego el carrito de compras

--- Proyecto/admin/logout.php
+++ Proyecto/admin/logout.php
@@ -1,4 +1,15 @@
 <?php
-  require_once('includes/load.php');
-  if(!$session->logout()) {redirect("index.php");}
+session_start();
+// ---
+// la tarea de este archivo es eliminar todo rastro de cookie
+
+// -- eliminamos el usuario
+if(isset($_SESSION['admin_id'])){
+	unset($_SESSION['admin_id']);
+}
+
+session_destroy();
+// v0 29 jul 2013
+//estemos donde estemos nos redirije al index
+print "<script>window.location='./';</script>";
 ?>
